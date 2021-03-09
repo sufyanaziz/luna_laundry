d6452a66381d50f8380620c2e7a8fc5a42fd8c69
Achmad Sufyan Aziz <49118546+sufyanaziz@users.noreply.github.com>
2021-03-09 16:57:43 +0700

    update index.tsx

--- src/index.tsx
+++ src/index.tsx
@@ -3,10 +3,15 @@ import ReactDOM from "react-dom";
 import App from "./App";
 
 import { BrowserRouter as Router } from "react-router-dom";
+import { UserProvider, StoreProvider } from "context";
 
 ReactDOM.render(
-  <Router>
-    <App />
-  </Router>,
+  <StoreProvider>
+    <UserProvider>
+      <Router>
+        <App />
+      </Router>
+    </UserProvider>
+  </StoreProvider>,
   document.getElementById("root")
 );
