0e29d2ac03614b5686c8d40ee22513031e20d45e
Achmad Sufyan Aziz <49118546+sufyanaziz@users.noreply.github.com>
2021-03-09 21:24:06 +0700

    Add day, month, year

--- src/views/user/pickupDate.tsx
+++ src/views/user/pickupDate.tsx
@@ -1,4 +1,4 @@
-import React from "react";
+import React, { useState } from "react";
 import styled from "styled-components";
 
 import { ComponentLayout } from "components/layout";
@@ -6,10 +6,14 @@ import { Card } from "components/global/Card";
 import { RouteComponentProps } from "react-router-dom";
 import { SelectInput, TextField } from "components/global/Input";
 import { Button } from "components/global/Button";
+import { getFullDays, getMonths, generateYear, formatDate } from "utils/date";
 
 interface PickupDateProps extends RouteComponentProps {}
 
 const PickupDate: React.FC<PickupDateProps> = ({ history }) => {
+  const [day, setDay] = useState<string>("");
+  const [month, setMonth] = useState<string>("");
+  const [year, setYear] = useState<string>("");
   return (
     <ComponentLayout isLogin={true}>
       <StyledPickupDate>
@@ -20,13 +24,35 @@ const PickupDate: React.FC<PickupDateProps> = ({ history }) => {
             </div>
             <div className="pickupDate-card__select-fields">
               <div className="pickupDate-card__select-field">
-                <SelectInput onChange={e => ""} values={["Date"]} value="" />
+                <SelectInput
+                  value={day}
+                  onChange={e => setDay(e.target.value)}
+                  values={getFullDays()}
+                  label="Day"
+                  isRemoveLabel={day !== "" ? true : false}
+                />
               </div>
               <div className="pickupDate-card__select-field">
-                <SelectInput onChange={e => ""} values={["Month"]} value="" />
+                <SelectInput
+                  value={month}
+                  onChange={e => setMonth(e.target.value)}
+                  values={getMonths()}
+                  label="Month"
+                  isRemoveLabel={month !== "" ? true : false}
+                />
               </div>
               <div className="pickupDate-card__select-field">
-                <SelectInput onChange={e => ""} values={["Year"]} value="" />
+                <SelectInput
+                  value={year}
+                  onChange={e => setYear(e.target.value)}
+                  values={generateYear(
+                    formatDate().getFullYear(),
+                    formatDate().getFullYear() + 2
+                  )}
+                  sort={{ by: "ASC", value: "num" }}
+                  label="Year"
+                  isRemoveLabel={year !== "" ? true : false}
+                />
               </div>
             </div>
             <div className="pickupDate-card__header">
